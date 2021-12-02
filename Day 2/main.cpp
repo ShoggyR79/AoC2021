dc0a29a6f7783ec9c9014df364ad510692a02dd1
Du Duong <khaidu2002@gmail.com>
2021-12-01 23:32:22 -0600

    day 2 done

--- Day 2/main.cpp
+++ Day 2/main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <map>
+
+void updateDirection(int &hor, int &vert, std::string dir, int val, int &aim){
+    if (dir == "forward"){
+        hor += val;
+        vert += (aim * val);
+    }
+    if (dir == "down"){
+        aim += val;
+    }
+    if (dir == "up"){
+        aim -= val;
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::ifstream infile;
+    infile.open("../input.txt");
+
+
+//    std::map<std::string, int>direction;
+    int horizontal = 0;
+    int vertical = 0;
+    int aim = 0;
+    std::string dir;
+    std::string value;
+    while (infile >> dir){
+        infile >> value;
+        updateDirection(horizontal, vertical, dir, std::stoi(value), aim);
+    }
+    std::cout<< horizontal<<" "<<vertical<<" "<< horizontal*vertical<<std::endl;
+
 }
+
+
